548714f88d95290ff93ac6474d560041b899915c
sen <>
2023-02-03 20:54:20 +0800

    examples

--- examples/x_douban_book_id.sh
+++ examples/x_douban_book_id.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat tag | ./crawler -u "https://book.douban.com/tag/{}?start={}&type=T" \
--s "#subject_list > ul > li > div.info > h2 > a" \
--a attr:href -p @,0 -pp @,20 -np @,+20 -ep @,980 -X \
+-s "#subject_list > ul > li > div.info > h2 > a[href] | cut / 1" \
+-p @,0 -pp @,20 -np @,+20 -ep @,980 -X \
 -sr 1-5,1-3 -lsr 20-50,15-30 \
 -px socks5://localhost:12311 \
 -ss -sc "没有找到符合条件的图书"
